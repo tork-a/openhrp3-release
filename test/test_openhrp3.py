a4d98cf439385cc8e0409f896542d57afd228e22
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2014-10-04 22:04:50 +0900

    Imported upstream version '3.1.7' of 'upstream'

--- test/test_openhrp3.py
+++ test/test_openhrp3.py
@@ -49,35 +49,38 @@ class TestCompile(unittest.TestCase):
         pkg_ret = os.path.exists(pkg_path)
         self.assertTrue(pkg_ret, "`rospack find openhrp3`(%s) returns %r"%(pkg_path, pkg_ret))
 
-    def check_if_file_exites_from_prefix(self, fname):
+    def check_if_file_exists_from_prefix(self, fname):
         self.check_if_file_exists("prefix", fname)
 
     def test_files_for_hrpsys(self):
         # https://github.com/start-jsk/hrpsys/blob/master/catkin.cmake#L125
-        self.check_if_file_exists_from_rospack("share/OpenHRP-3.1/sample/project")
-        # self.check_if_file_exites_from_prefix("share/openhrp3/share/OpenHRP-3.1/sample/project")
+        # self.check_if_file_exists_from_rospack("share/OpenHRP-3.1/sample/project")
+        self.check_if_file_exists_from_prefix("share/OpenHRP-3.1/sample/project")
 
         # https://code.google.com/p/hrpsys-base/source/browse/trunk/idl/CMakeLists.txt#118
         self.check_if_file_exists("idl_dir",            "OpenHRP/OpenHRPCommon.idl")
         # https://code.google.com/p/hrpsys-base/source/browse/trunk/sample/PA10/PA10.conf.in#1
-        self.check_if_file_exists_from_rospack("share/OpenHRP-3.1/sample/model/PA10/pa10.main.wrl")
+        # self.check_if_file_exists_from_rospack("share/OpenHRP-3.1/sample/model/PA10/pa10.main.wrl")
 
     def test_files_for_hrpsys_ros_bridge(self):
         # https://github.com/start-jsk/rtmros_common/blob/master/hrpsys_ros_bridge/test/test-samplerobot.py#L63
-        self.check_if_file_exists_from_rospack("share/OpenHRP-3.1/sample/controller/SampleController/etc/Sample.pos")
+        # self.check_if_file_exists_from_rospack("share/OpenHRP-3.1/sample/controller/SampleController/etc/Sample.pos")
+        self.check_if_file_exists_from_prefix("share/OpenHRP-3.1/sample/controller/SampleController/etc/Sample.pos")
 
         # https://github.com/start-jsk/rtmros_common/blob/master/hrpsys_ros_bridge/catkin.cmake#L141
         self.check_if_file_exists("idl_dir",            "../sample/model/PA10/pa10.main.wrl")
         self.check_if_file_exists("idl_dir",            "../sample/model/sample1.wrl")
-        self.check_if_file_exists_from_rospack("share/OpenHRP-3.1/sample/model/PA10/pa10.main.wrl")
-        self.check_if_file_exists_from_rospack("share/OpenHRP-3.1/sample/model/sample1.wrl")
+        self.check_if_file_exists_from_prefix("share/OpenHRP-3.1/sample/model/PA10/pa10.main.wrl")
+        self.check_if_file_exists_from_prefix("share/OpenHRP-3.1/sample/model/sample1.wrl")
+        # self.check_if_file_exists_from_rospack("share/OpenHRP-3.1/sample/model/PA10/pa10.main.wrl")
+        # self.check_if_file_exists_from_rospack("share/OpenHRP-3.1/sample/model/sample1.wrl")
 
     ## test 1 == 1
     def test_compile_pkg_config(self):
         global PID
         cmd = "%s pkg-config openhrp3.1 --cflags --libs"%(self.PKG_CONFIG_PATH)
         print "`"+cmd+"` =",check_output(cmd, shell=True, stderr=STDOUT)
-        ret = call("gcc -o openhrp3-sample-pkg-config /tmp/%d-openhrp3-sample.cpp `%s`"%(PID,cmd), shell=True)
+        ret = call("g++ -o openhrp3-sample-pkg-config /tmp/%d-openhrp3-sample.cpp `%s`"%(PID,cmd), shell=True)
         self.assertTrue(ret==0)
 
     def _test_compile_move_ankle(self):
@@ -85,7 +88,7 @@ class TestCompile(unittest.TestCase):
         cmd2 = "pkg-config openhrp3.1 --variable=idl_dir"
         print "`"+cmd1+"` =",check_output(cmd1, shell=True, stderr=STDOUT)
         print "`"+cmd2+"` =",check_output(cmd2, shell=True, stderr=STDOUT)
-        ret = call("gcc -o move_ankle `%s`/../sample/example/move_ankle/move_ankle.cpp `%s`"%(cmd2,cmd1), shell=True)
+        ret = call("g++ -o move_ankle `%s`/../sample/example/move_ankle/move_ankle.cpp `%s`"%(cmd2,cmd1), shell=True)
         self.assertTrue(ret==0)
 
     def test_idl_dir(self):
@@ -102,6 +105,12 @@ class TestCompile(unittest.TestCase):
         print "`"+cmd+"`"+fname+" = "+os.path.join(check_output(cmd, shell=True).rstrip(), fname)
         self.assertTrue(os.path.exists(os.path.join(check_output(cmd, shell=True).rstrip(), fname)))
 
+        cmd = "%s pkg-config openhrp3.1 --variable=prefix"%(self.PKG_CONFIG_PATH)
+        fname = "share/OpenHRP-3.1/sample/model/PA10/pa10.main.wrl"
+        # check if model file exists
+        print "`"+cmd+"`"+fname+" = "+os.path.join(check_output(cmd, shell=True).rstrip(), fname)
+        self.assertTrue(os.path.exists(os.path.join(check_output(cmd, shell=True).rstrip(), fname)))
+
     def test_sample_samplerobot(self):
         cmd = "%s pkg-config openhrp3.1 --variable=idl_dir"%(self.PKG_CONFIG_PATH)
         fname = "../sample/model/sample1.wrl"
@@ -111,6 +120,17 @@ class TestCompile(unittest.TestCase):
         #
         # check if walk data file exists
         fname = "../sample/controller/SampleController/etc/Sample.pos"
+        print "`"+cmd+"`"+fname+" = "+os.path.join(check_output(cmd, shell=True).rstrip(), fname)
+        self.assertTrue(os.path.exists(os.path.join(check_output(cmd, shell=True).rstrip(), fname)), "cmd = %r, fname = %r"%(cmd, fname))
+
+        cmd = "%s pkg-config openhrp3.1 --variable=prefix"%(self.PKG_CONFIG_PATH)
+        fname = "share/OpenHRP-3.1/sample/model/sample1.wrl"
+        # check if model file exists
+        print "`"+cmd+"`"+fname+" = "+os.path.join(check_output(cmd, shell=True).rstrip(), fname)
+        self.assertTrue(os.path.exists(os.path.join(check_output(cmd, shell=True).rstrip(), fname)), "cmd = %r, fname = %r"%(cmd, fname))
+        #
+        # check if walk data file exists
+        fname = "share/OpenHRP-3.1/sample/controller/SampleController/etc/Sample.pos"
         print "`"+cmd+"`"+fname+" = "+os.path.join(check_output(cmd, shell=True).rstrip(), fname)
         self.assertTrue(os.path.exists(os.path.join(check_output(cmd, shell=True).rstrip(), fname)), "cmd = %r, fname = %r"%(cmd, fname))
 

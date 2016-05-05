6ab8316e711a457936921c63c740f62a835ee92d
Kei Okada <kei.okada@gmail.com>
2016-05-05 17:03:20 +0900

    Rebase from 'rpm/kinetic/openhrp3'

--- hrplib/hrpPlanner/TimeUtil.cpp
+++ hrplib/hrpPlanner/TimeUtil.cpp
@@ -8,6 +8,10 @@ tick_t get_tick()
     LARGE_INTEGER t;
     QueryPerformanceCounter(&t);
     return t.QuadPart;
+#elif defined(__ARM_ARCH_7A__)
+    uint32_t r = 0;
+    asm volatile("mrc p15, 0, %0, c9, c13, 0" : "=r"(r) );
+    return r;
 #elif defined(__AARCH64EL__)
     uint64_t b;
     asm volatile( "mrs %0, pmccntr_el0" : "=r"(b) :: "memory" );
